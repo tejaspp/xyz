1064590bf8765bf7fb98f9e21fc79602fff8925c
tejaspp <tejas.techno@gmail.com>
2017-11-27 14:31:12 +0900

    Revised commit

--- hello.cpp
+++ hello.cpp
@@ -1,2 +1,9 @@
 /* new project
 */
+
+//My name is Anthony Gonsalves
+//Main duniya mein akelaa hoon
+//Dil bhi hain khaali, Bill bhi hain khaali
+//Isme rahegi koi kismat waali
+//Jise meri yaad aaye jab chaahe chali aaye
+//Gali no. 420, Excuse me please
